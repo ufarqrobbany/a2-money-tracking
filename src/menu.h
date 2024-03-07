06a5cf96ae159302967260c7fcc11b498b8ed5d4
oswyongie <yunindanadia16@gmail.com>
2024-03-07 20:43:27 +0700

    update menu lagi

--- src/menu.h
+++ src/menu.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 void tampilMenuAwal();
 void tampilMenuLogin();
 void tampilMenuDaftar();
@@ -7,3 +8,6 @@ void tampilMenuUtama(char username[20]);
 void tampilMenuCatat(char username[20]);
 void tampilMenuRekap(char username[20]);
 void tampilMenuDompet(char username[20]);
+void tampilMenuTambahDompet(char username[20]);
+void tampilMenuHapusDompet(char username[20]);
+void tampilKonfirmasiHapusDompet(char username[20], char namadompet[20], int iddompet);
