f1e89b4d5a2888a2f92ce6cd079e8781f3c359b1
ufarqrobbany <faruqrobbany@gmail.com>
2024-03-07 19:45:01 +0700

    add hapusDompet

--- src/menu.h
+++ src/menu.h
@@ -7,3 +7,6 @@ void tampilMenuUtama(char username[20]);
 void tampilMenuCatat(char username[20]);
 void tampilMenuRekap(char username[20]);
 void tampilMenuDompet(char username[20]);
+void tampilMenuTambahDompet(char username[20]);
+void tampilMenuHapusDompet(char username[20]);
+void tampilKonfirmasiHapusDompet(char username[20], char namadompet[20], int iddompet);
